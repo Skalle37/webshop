84d6ae96bf71f3f4d79a61461d05d92b38031400
Skalle37 <kallevallner@gmail.com>
2021-05-11 15:21:01 +0300

    itemservice, uue eseme lisamine, eseme list, eseme kustutamine

--- src/app/admin/item-add/item-add.component.ts
+++ src/app/admin/item-add/item-add.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ItemService } from 'src/app/services/item.service';
 
 @Component({
   selector: 'app-item-add',
@@ -8,18 +9,16 @@ import { NgForm } from '@angular/forms';
 })
 export class ItemAddComponent implements OnInit {
 
-  constructor() { }
+  constructor( private itemService: ItemService) { }
 
   ngOnInit(): void {
+   // this.form = this.form.value; 
   }
 
   onSubmit(form: NgForm) { 
     alert(form);
-    console.log(form);
-    console.log(form.value);
-    console.log(form.value.title);
-    console.log(form.value.price);
-    
-
+    if (form.valid) {
+      this.itemService.items.push(form.value);
+    } 
   }
 }
